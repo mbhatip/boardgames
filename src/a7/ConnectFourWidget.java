f5875794afc62637af7679fa16f20aed6da670f5
Memohat <49382701+Memohat@users.noreply.github.com>
2019-11-24 21:37:28 -0500

    concise and better code style with comments

--- src/a7/ConnectFourWidget.java
+++ src/a7/ConnectFourWidget.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -13,28 +15,26 @@ import a7.JSpotBoard.BoardStyle;
 
 public class ConnectFourWidget extends JPanel implements ActionListener, SpotListener{
 
-	// Enum to identify player
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private enum Player {RED, BLACK};
+	private enum Player {RED, BLACK};	// Enum to identify player
 	
 	private JSpotBoard _board;		//SpotBoard playing area
 	private JLabel _message;		// Label for messages
 	private boolean _gameWon;		// Game Won boolean
 	private Player _nextToPlay;		// Turn of player 2
+	private Spot _selection;		// Selected spot
 	
 	public ConnectFourWidget() {
 		
-		// Create SpotBoard and message label
+		// Create SpotBoard and message label, initialize selection spot
 		_board = new JSpotBoard(7,6, BoardStyle.COLUMNS);
 		_message = new JLabel();
-		
-		// Create player boards
-		
+		_selection = null;
 		
 		// Set layout and place spotboard at center
 		setLayout(new BorderLayout());
@@ -93,177 +93,171 @@ public class ConnectFourWidget extends JPanel implements ActionListener, SpotLis
 		String player2 = "Black";
 		Color playerColor = Color.RED;
 		
+		// Checking to see if players need to be changed
 		if(_nextToPlay == Player.BLACK) {
 			player1 = "Black";
 			player2 = "Red";
 			playerColor = Color.BLACK;
 			_nextToPlay = Player.RED;
 		}
-		else { _nextToPlay = Player.BLACK;}
+		else { _nextToPlay = Player.BLACK;}	
 		
-		// Set color of spot, toggle to color
+		// Setting global selection spot variable equal to s
+		_selection = s;
+		
+		// Only allow empty spot at bottom of each column to be changed
+		// Selected spot becomes the first empty spot
+		// If no spots are available, return
 		for (int y = _board.getSpotHeight()-1; y >= 0; y--) {
-			Spot spot = _board.getSpotAt(s.getSpotX(), y);
-			if (spot.isEmpty()) { s = spot; break;}
+			Spot spot = _board.getSpotAt(_selection.getSpotX(), y);
+			if (spot.isEmpty()) { _selection = spot; break;}
 			if (y == 0) {return;}
 		}
 		
-		s.setSpotColor(playerColor);
-		s.toggleSpot();
-		spotExited(s);
-		spotEntered(s);
+		// Change color of spot and highlights of column
+		_selection.setSpotColor(playerColor);
+		_selection.toggleSpot();
+		spotExited(_selection);
+		spotEntered(_selection);
 		
-		checkWin(s);
+		// Check to see if game is won
+		checkWin();
 		
-		// Someone won the game
 		if (_gameWon) {
 			// Setting won message
 			_message.setText(player1 + " Wins!");
 		}
 		else {
+			// Checking to see if draw game
 			boolean drawGame = true;
 			for (Spot spot : _board) {
 				if (spot.isEmpty()) {drawGame = false; break;}
 			}
+			
+			// Message for draw game or for next turn
 			if (drawGame) { _message.setText("Draw Game."); _gameWon = true;}
 			else {_message.setText(player2 + " to play."); }
 		}
 	}
 	
-	private void checkWin(Spot s) {
-		Color playerColor = s.getSpotColor();
+	private void checkWin() {
+		// Checking game won variable
+		if (_gameWon) {return;}
 		
-		// Checking each column and row
+		// Checking each row
+		checkRows();
+		if (_gameWon) {return;}
 		
-		int width = _board.getSpotWidth();
-		int height = _board.getSpotHeight();
-		
-		for (int dimension = 0; dimension < 2; dimension++) {
-			boolean checkRows = dimension == 1;
-			int limit1 = checkRows ? height : width;
-			int limit2 = checkRows ? width : height;
-			
-			for (int var1 = 0; var1 < limit1; var1++) {
-				int counter = 0;
-				Spot start = null;
-				for (int var2 = 0; var2 < limit2; var2++) {
-					int x = checkRows ? var2 : var1;
-					int y = checkRows ? var1 : var2;
-					Spot spot = _board.getSpotAt(x, y);
-					if (spot.isEmpty() || playerColor != spot.getSpotColor()) {
-						counter = 0;
-						continue;
-					}
-					if (counter == 0) {
-						start = spot;
-					}
-					counter++;
-					
-					if (counter >= 4) {
-						spotExited(s);
-						_gameWon = true;
-						for (int i = 0; i < counter; i++) {
-							int row = start.getSpotY();
-							int col = start.getSpotX();
-							if (checkRows) { col += i; }
-							else { row += i;}
-							_board.getSpotAt(col, row).highlightSpot();
-						}
-						return;
-					}
-				}
-			}
-		}
+		// Checking each column
+		checkCols();
+		if (_gameWon) {return;}
 		
 		// Checking diagonals
-		for (int i = 0; i < 12; i++) {
-			int counter = 0;
-			Spot start = null;
-			boolean leftToRight = i < 6;
-			int x;
-			int y;
+		checkDiagonals();
+		if (_gameWon) {return;}
+		
+	}
+	
+	/* checkRows iterates through each possible row in the board and passes
+	 * them to the checkListOfSpots function. Returns if _gameWon is true
+	 * or out of rows
+	 */
+	
+	private void checkRows() {
+		List<Spot> row = new ArrayList<>();
+		for (Spot s : _board) {
+			row.add(s);
+			if (s.getSpotX() >= _board.getSpotWidth()-1) {
+				checkListOfSpots(row);
+				if (_gameWon) {return;}
+				row.clear();
+			}
+		}
+	}
+	
+	/* checkCols iterates through each possible column in the board and passes
+	 * them to the checkListOfSpots function. Returns if _gameWon is true
+	 * or out of columns
+	 */
+	
+	private void checkCols() {
+		List<Spot> col = new ArrayList<>();
+		for (int x = 0; x < _board.getSpotWidth(); x++) {
+			for (int y = 0; y < _board.getSpotHeight(); y++) {
+				col.add(_board.getSpotAt(x, y));
+			}
+			checkListOfSpots(col);
+			if (_gameWon) {return;}
+			col.clear();
+		}
+	}
+	
+	/* checkDiagonals iterates through each possible diagonal in the board and passes
+	 * them to the checkListOfSpots function. Returns if _gameWon is true
+	 * or out of diagonals
+	 */
+	private void checkDiagonals() {
+		boolean leftToRight = true;
+		for (int i = 0; i < 6; i++) {
+			List<Spot> diagonal = new ArrayList<>();
+			int x; int y;
 			if (leftToRight) {
 				x = i < 3 ? 0 : i-2;
 				y = i < 3 ? i+3 : 5;
 			}
 			else {
-				i -= 6;
 				x = i < 3 ? i+3 : 6;
 				y = i < 3 ? 5 : 8-i;
-				i += 6;
 			}
 			while (true) {
-				if (!validXY(x,y)) {
-					break;
-				}
-				Spot spot = _board.getSpotAt(x, y);
-				x--; y--;
-				if (leftToRight) {x += 2;}
-				
-				if (spot.isEmpty() || playerColor != spot.getSpotColor()) {
-					counter = 0;
-					continue;
-				}
-				if (counter == 0) {
-					start = spot;
-				}
-				counter++;
-				if (counter >= 4) {
-					spotExited(s);
-					_gameWon = true;
-					for (int j = 0; j < counter; j++) {
-						int col = start.getSpotX();
-						int row = start.getSpotY();
-						if (leftToRight) {col += 2*j;}
-						_board.getSpotAt(col - j, row - j).highlightSpot();
-					}
-					return;
-				}
+				if (invalidXY(x,y)) {break;}
+				diagonal.add(_board.getSpotAt(x, y));
+				if (leftToRight) {x++;} else {x--;} y--;
+			}
+			checkListOfSpots(diagonal);
+			if (_gameWon) {return;}
+			
+			if (i == 5) {
+				leftToRight = !leftToRight;
+				if (leftToRight) {break;}
+				i = -1;
 			}
 		}
-		
-		// Checking diagonals from bottom right to top left
-		/*
-		for (int i = 0; i < 6; i++) {
-			int counter = 0;
-			Spot start = null;
-			int x = i < 3 ? i+3 : 6;
-			int y = i < 3 ? 5 : 8-i;
-			
-			while (true) {
-				if (!validXY(x,y)) {
-					break;
-				}
-				Spot spot = _board.getSpotAt(x, y);
-				x--; y--;
-				
-				if (spot.isEmpty() || playerColor != spot.getSpotColor()) {
-					counter = 0;
-					continue;
-				}
-				if (counter == 0) {
-					start = spot;
-				}
-				counter++;
-				
-				if (counter >= 4) {
-					spotExited(s);
-					_gameWon = true;
-					for (int j = 0; j < counter; j++) {
-						int column = start.getSpotX();
-						int row = start.getSpotY();
-						_board.getSpotAt(column - j, row - j).highlightSpot();
-					}
-					return;
-				}
-			}
-		}*/
 	}
 	
-	private boolean validXY(int x, int y) {
-		return !(x < 0 || x == _board.getSpotWidth() || y < 0 || y == _board.getSpotHeight());
+	/*
+	 * Checks to see if the passed x and y values are out of bounds, used by checkDiagonals 
+	 * function	
+	 */
+	
+	private boolean invalidXY(int x, int y) {
+		return (x < 0 || x >= _board.getSpotWidth() || y < 0 || y >= _board.getSpotHeight());
 	}
-		
+	
+	/* 
+	 * Checks to see if list of spots passed to it match the selection color and have 4 in a 
+	 * row. If there is a 4 in a row, highlights each spot and makes global win variable true,
+	 * then returns
+	 */
+	
+	private void checkListOfSpots(List<Spot> spots) {
+		List<Spot> winners = new ArrayList<Spot>();
+		for (Spot spot : spots) {
+			winners.add(spot);
+			if (spot.isEmpty() || _selection.getSpotColor() != spot.getSpotColor()) {
+				winners.clear();
+			}
+			if (winners.size() >= 4) {
+				spotExited(_selection);
+				for (Spot s : winners) {
+					s.highlightSpot();
+				}
+				_gameWon = true;
+				return;
+			}
+			
+		}
+	}
 	
 
 	public void spotEntered(Spot s) {
